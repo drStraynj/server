f3b78e9a60a153bc1aec22ade0e7019b490d94d3
Danny_C <danny.cox@aunalytics.com>
2014-06-16 16:31:27 -0400

    Committing changes so I can push to different machine

--- server.js
+++ server.js
@@ -5,40 +5,42 @@ module.exports = _server;
 function _server() {
   var express = require('express'),
     app = express(),
-    fs = require('fs'),
     chokidar = require('chokidar'),
     path = require("path"),
+    fs = require('fs'),
     self = u.chain(u.eventEmitter({}));
 
+  self.var("port", "80")
   self.var("views", path.resolve("./views"), function(_) {
     self._vars.views = path.resolve(_);
-  });
+  })
   self.var("public", path.resolve("./public"), function(_) {
-    self._vars.public = path.resolve(_);
-  });
-  self.var("viewEngine", "jade");
-  self.var("port", "80");
+    self._vars.public = path.resolve(_)
+  })
+  self.var('home', function(req, res) {
+    res.send("UNDER CONSTRUCTION")
+  })
 
   self.applyConfiguration = function() {
     app.set('port', self.port())
-    app.set('views', self.views());
-    app.set('view engine', self.viewEngine());
-    app.use(require("compression"));
-    app.use('/public', express.static(self.public()));
+    app.set('views', self.views())
+    app.engine('.jade', require('jade').__express)
+    app.use('/public', express.static(self.public()))
+    app.get('/', self.home())
 
     chokidar.watch(self.views(), { ignored:/^\./, persistent: true })
       .on("add", function(file) {
-        var name = '/' + path.basename(file).replace(path.extname(file), '');
-        app.get(name, function(req, res) {
-          console.log(res);
-        });
-      });
-  };
-
+          var name = getName(file);
+          if (name == "/home") name = '/';
+          app.get(name, function(req, res) { res.render(file) })
+        })
+  }
   self.start = function() {
     self.applyConfiguration();
-    console.log(self.port());
     app.listen(self.port());
-  };
+  }
   return self;
+  function getName(file) {
+    return '/' + path.basename(file).replace(path.extname(file), '');
+  }
 }
